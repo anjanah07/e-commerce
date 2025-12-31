f7eb35fd8f40856d41cf2728975747aa6986416a
anjanah07 <thanjana7@gmail.com>
2025-12-31 12:24:25 +0530

    edit : metadata

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Home",
+};
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+  );
+}
