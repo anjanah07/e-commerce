0f54a32e9a01a94172f8e058219b79b0f42166e5
anjanah07 <thanjana7@gmail.com>
2026-01-01 22:18:08 +0530

    add: header

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { ShoppingCart, UserIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="flex flex-row justify-between">
+        <div className="flex-start">
+          <Link href={"/"} className="flex flex-row items-center justify-start">
+            <Image
+              src={"/images/logo.svg"}
+              alt="Logo image"
+              height={48}
+              width={48}
+              priority
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant={"ghost"}>
+            <Link href={"/cart"}>
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/signin"}>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
