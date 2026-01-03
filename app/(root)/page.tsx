9d15c36bcca5a72b82f3ad65c41de09eef96ad0a
anjanah07 <thanjana7@gmail.com>
2026-01-03 11:48:38 +0530

    add: not found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ export const metadata = {
 };
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export default async function Home() {
-  await delay(4000);
+  await delay(2000);
   return (
     <div className="flex h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
   );
