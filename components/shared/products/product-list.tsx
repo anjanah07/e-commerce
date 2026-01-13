6c7177675290a0218dff37b9365e34183c1bdb21
anjanah07 <thanjana7@gmail.com>
2026-01-13 22:39:14 +0530

    add: product type; zod validation

--- components/shared/products/product-list.tsx
+++ components/shared/products/product-list.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/types";
 import ProductCard from "./product-card";
 
 const ProductList = ({
@@ -5,7 +6,7 @@ const ProductList = ({
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) => {
@@ -15,7 +16,7 @@ const ProductList = ({
       <h2 className="h2-bold mb-4">{title}</h2>
       {limitedData.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-          {data.map((product: any) => (
+          {data.map((product: Product) => (
             <ProductCard product={product} key={product.slug} />
           ))}
         </div>
---
+++ lib/validators.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { formatNumberWithDecimal } from "./utils";
+const currency = z
+  .string()
+  .refine(
+    (val) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(val))),
+    "Price must have exactly two decimal places"
+  );
+export const insertProductSchema = z.object({
+  name: z.string().min(3, "Name must be atleast 3 characters"),
+  slug: z.string().min(3, "Slug must be atleast 3 characters"),
+  category: z.string().min(3, "Category must be atleast 3 characters"),
+  brand: z.string().min(3, "Brand must be atleast 3 characters"),
+  description: z.string().min(3, "Description must be atleast 3 characters"),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, "Product must have atleast one image"),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: currency,
+});
