83d7aa63ae13fa74f4e6fd85ea27586b50bf55ac
anjanah07 <thanjana7@gmail.com>
2026-01-14 21:25:46 +0530

    setup: neon serverless w prisma

--- lib/actions/products.actions.ts
+++ lib/actions/products.actions.ts
@@ -3,19 +3,25 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "../generated/prisma/client";
 import { convertToPlainObject } from "../utils";
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+  log: ["info", "query"],
+});
 
 export const getLatestProducts = async () => {
-  const adapter = new PrismaNeon({
-    connectionString: process.env.DATABASE_URL!,
-  });
-
-  const prisma = new PrismaClient({
-    adapter,
-    log: ["info", "query"],
-  });
   const data = await prisma.product.findMany({
     take: 4,
     orderBy: { createdAt: "desc" },
   });
   return convertToPlainObject(data);
+};
+
+export const getProductBySlug = async (slug: string) => {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 };
